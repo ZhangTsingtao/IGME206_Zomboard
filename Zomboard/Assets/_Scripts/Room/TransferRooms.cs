4ace53655e732b069aeac2a5b38a705d6aae7e0a
Shaxiipoo <Hulse530@gmail.com>
2023-12-15 00:43:34 -0500

    Added room transfer

---
+++ Zomboard/Assets/_Scripts/Room/TransferRooms.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransferRooms : MonoBehaviour
+{
+    public string TargetScene;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Changing to " + TargetScene);
+            SceneManager.LoadScene(TargetScene);
+        }
+    }
+}
