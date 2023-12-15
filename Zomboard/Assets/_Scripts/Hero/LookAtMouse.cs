030c2c465d3c35abc8696b2eb9342b09204f6942
ZhangTsingtao <rickzhangqt@gmail.com>
2023-12-15 00:46:30 -0500

    Lock on zombie

--- Zomboard/Assets/_Scripts/Hero/LookAtMouse.cs
+++ Zomboard/Assets/_Scripts/Hero/LookAtMouse.cs
@@ -13,10 +13,21 @@ public class LookAtMouse : MonoBehaviour
 
         if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
         {
+            if (hit.collider.CompareTag("Zombie"))
+            {
+                LookAtTarget(hit.collider.transform.position);
+                return;
+            }
             //look
             Vector3 lookPoint = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             transform.LookAt(lookPoint);
             raycastPosition = hit.point;
         }
+    }
+
+    private void LookAtTarget(Vector3 target)
+    {
+        Vector3 lookPoint = new Vector3(target.x, transform.position.y, target.z);
+        transform.LookAt(lookPoint);
     }
 }
